3017b810444e755229e3698acd44d8f22f4af98b
Kelton Karboviak <kelton.karboviak@gmail.com>
2019-12-29 12:58:01 -0700

    Update project files (#6)
    * Remove py35 from tox.ini
    
    * Add pyproject.toml
    
    * Blackify
    
    * Remove unneeded shebang
    
    * Add build & release parts to version scheme
    
    * Fix Travis CI link in README
    
    * Move package code into src/ dir
    
    * Add towncrier dependency
    
    * Add towncrier config

--- setup.py
+++ setup.py
@@ -5,41 +5,44 @@
 from setuptools import setup, find_packages
 
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
 
-with open('HISTORY.md') as history_file:
+with open("HISTORY.md") as history_file:
     history = history_file.read()
 
 
-requirements = ['pydantic>=1.2']
+requirements = ["pydantic>=1.2"]
 
 
 setup(
     author="Kelton Karboviak",
-    author_email='kelton.karboviak@gmail.com',
-    python_requires='>=3.6',
+    author_email="kelton.karboviak@gmail.com",
+    python_requires=">=3.6",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Pydantic models for AWS Events",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
-    long_description_content_type='text/markdown',
+    long_description=readme + "\n\n" + history,
+    long_description_content_type="text/markdown",
     include_package_data=True,
-    keywords='AWS Lambda Pydantic',
-    name='aws-lambda-event-models',
-    packages=find_packages(include=['aws_lambda_event_models', 'aws_lambda_event_models.*']),
-    url='https://github.com/KeltonKarboviak/aws-lambda-event-models',
-    version='0.0.1',
+    keywords="AWS Lambda Pydantic",
+    name="aws-lambda-event-models",
+    package_dir={"": "src"},
+    packages=find_packages(
+        where="src", include=["aws_lambda_event_models", "aws_lambda_event_models.*"]
+    ),
+    url="https://github.com/KeltonKarboviak/aws-lambda-event-models",
+    version="0.0.1",
     zip_safe=False,
 )
--- tests/aws_lambda_event_models_test.py
+++ tests/aws_lambda_event_models_test.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 """Tests for `aws-lambda-event-models` package."""
 
 import aws_lambda_event_models
@@ -7,4 +5,4 @@ import aws_lambda_event_models
 
 def test_version():
     """Sample pytest test function with the pytest fixture as an argument."""
-    assert '0.0.1' == aws_lambda_event_models.__version__
+    assert "0.0.1" == aws_lambda_event_models.__version__
