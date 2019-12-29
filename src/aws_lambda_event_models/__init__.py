b5859d4b23b278351f7a55453b39e77ffb7896c9
Kelton Karboviak <kelton.karboviak@daveramsey.com>
2019-12-29 11:40:08 -0800

    Fill out rest of S3 Event Model

--- src/aws_lambda_event_models/__init__.py
+++ src/aws_lambda_event_models/__init__.py
@@ -1,5 +1,6 @@
 """Top-level package for aws-lambda-event-models."""
 
+from datetime import datetime
 from enum import Enum
 
 from pydantic import BaseModel
@@ -14,9 +15,47 @@ class EventSource(str, Enum):
     s3 = "aws:s3"
 
 
+class S3Bucket(BaseModel):
+    name: str
+    arn: str
+
+
+class S3Object(BaseModel):
+    key: str
+    size: int
+    etag: str
+    version_id: str
+    sequencer: str
+
+    class Config:
+        fields = {
+            "etag": "eTag",
+            "version_id": "versionId",
+        }
+
+
+class S3(BaseModel):
+    schema_version: str
+    bucket: S3Bucket
+    object: S3Object
+
+    class Config:
+        fields = {"schema_version": "s3SchemaVersion"}
+
+
 class S3EventRecord(BaseModel):
     event_version: str
     event_source: EventSource
+    aws_region: str
+    event_time: datetime
+    event_name: str
+    s3: S3
 
     class Config:
-        fields = {"event_version": "eventVersion", "event_source": "eventSource"}
+        fields = {
+            "event_version": "eventVersion",
+            "event_source": "eventSource",
+            "aws_region": "awsRegion",
+            "event_time": "eventTime",
+            "event_name": "eventName",
+        }
--- tests/aws_lambda_event_models_test.py
+++ tests/aws_lambda_event_models_test.py
@@ -1,5 +1,7 @@
 """Tests for `aws-lambda-event-models` package."""
 
+from datetime import datetime, timezone
+
 import pytest
 
 from aws_lambda_event_models import EventSource, S3EventRecord
@@ -49,5 +51,23 @@ def s3_event_record():
 def test_correctly_parse_valid_s3_event_record(s3_event_record):
     """Test an S3 Event Record from AWS can be parsed into a Pydantic model."""
     record = S3EventRecord(**s3_event_record)
+
+    # top-level fields
     assert "2.1" == record.event_version
     assert "aws:s3" == record.event_source and EventSource.s3 == record.event_source
+    assert "us-west-2" == record.aws_region
+    assert datetime(1970, 1, 1, 0, 0, 0, tzinfo=timezone.utc) == record.event_time
+    assert "ObjectCreated:Put" == record.event_name
+
+    # s3 fields
+    s3_metadata = record.s3
+    assert "1.0" == s3_metadata.schema_version
+
+    assert "mybucket" == s3_metadata.bucket.name
+    assert "arn:aws:s3:::mybucket" == s3_metadata.bucket.arn
+
+    assert "HappyFace.jpg" == s3_metadata.object.key
+    assert 1024 == s3_metadata.object.size
+    assert "d41d8cd98f00b204e9800998ecf8427e" == s3_metadata.object.etag
+    assert "096fKKXTRTtl3on89fVO.nfljtsv6qko" == s3_metadata.object.version_id
+    assert "0055AED6DCD90281E5" == s3_metadata.object.sequencer
